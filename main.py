5e992f763a26a4d69f200eef23d088f9b8d5cb69
AmitKumar-Fexle-Training <amit.kumar@fexle.com>
2023-10-19 12:56:40 +0530

    commit 14

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[1]:
+
+
 import os
 from simple_salesforce import Salesforce, format_soql
 from pprint import pprint
@@ -14,6 +17,10 @@ import json
 import sys
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
+
+# In[2]:
+
+
 config = configparser.ConfigParser()
 
 # Read options from the CSV file into a DataFrame
@@ -23,7 +30,11 @@ options_df = pd.read_csv('Org Configs.csv')
 options = options_df.set_index('orgName')['configFileName'].to_dict()
 
 listOfChoices = []
-releaseName  = sys.argv[1] #input('Please enter the release Name: ') 
+releaseName  = sys.argv[1] #input('Please enter the release Name: ')
+
+
+# In[3]:
+
 
 while True:
     print("Select an option:")
@@ -41,12 +52,16 @@ while True:
     elif choice.isdigit() and int(choice) in range(1, len(options) + 1):
         selected_option = list(options.keys())[int(choice) - 1]
         print("You have selected:", selected_option)
-        confirmation = sys.argv[3] #input("Confirm your choice (Y/N): ")
+        confirmation = sys.argv[3] #input("Confirm your choice (Y/N): "
         if confirmation.lower() == 'y':
             break
     else:
         print("Invalid choice. Please try again.")
         break
+
+
+# In[4]:
+
 
 fileName = options[selected_option]+'.ini'
 
@@ -61,8 +76,51 @@ if os.path.isfile(fileName):
 else:
     print(f"The file '{fileName}' does not exist in the '{credsPath}' directory.")
 
+
+# In[5]:
+
+
+def get_story_tracker(folder_path, options_df):
+    config_list = []
+    for scriptFileName in os.listdir(folder_path):
+
+        # Initialize variables to store the extracted values
+        username = None
+        environments = []
+
+        file_path = os.path.join(folder_path, scriptFileName)
+
+        configDict = {}
+
+        orgNameList = list(options_df['orgName'])
+        for orgName in orgNameList:
+            configDict[orgName.upper()] = False
+
+        # Open the file and read its contents
+        with open(file_path, 'r') as file:
+            for line in file:
+                if line.startswith('//UserName:'):
+                    username = line.split(': ')[1].strip()
+                elif line.startswith('//Environment:'):
+                    environments = [env.strip() for env in line.split(': ')[1].split(',')]
+
+                configDict['Execute Script? (Admin Only)'] = True
+                configDict['Script File Name'] = scriptFileName.split('.')[0]
+                configDict['Run As UserName'] = username
+                for env in environments:
+                    if env.upper() in configDict:
+                        configDict[env.upper()] = True
+
+
+        config_list.append(configDict)
+    return pd.DataFrame(config_list)
+
+
+# In[6]:
+
+
 folder_path = 'manual-steps-automation/scripts/apex'
-configDF = pd.read_csv("manual-steps-automation/Story Tracker.csv")
+configDF = get_story_tracker(folder_path, options_df) #pd.read_csv("manual-steps-automation/Story Tracker.csv")
 configDF = configDF.sort_values(by='Script File Name', ascending=True)
 columnsToKeep = ['Execute Script? (Admin Only)', 'Script File Name', 'Run As UserName']
 columnsToKeep.append(selected_option.upper())
@@ -74,6 +132,9 @@ columnsToKeep.append(errorColumn)
 resultDf[successColumn] = None
 resultDf[errorColumn] = None
 configDF[errorColumn] = False 
+
+
+# In[8]:
 
 
 #secondary_path_folder_name = 'scripts\apex'
@@ -90,7 +151,7 @@ def read_files_in_folder(apex_class_with_changes, folder_path, configDF):
         for apex_class in apex_class_with_changes:
             configDF.loc[configDF['Script File Name'] == apex_class.split('.')[0], 'error'] = False 
     
-    failed_script_name = '' 
+    #failed_script_name = '' 
     # Loop over all files in the folder
     for scriptFileName in os.listdir(folder_path):
         file_name_without_extension = os.path.splitext(scriptFileName)[0]
@@ -136,7 +197,7 @@ def read_files_in_folder(apex_class_with_changes, folder_path, configDF):
                             resultDf.at[index, successColumn] = result['success']
                             print(f"Script not executed due to {result['exceptionMessage']}")
                             configDF.loc[configDF['Script File Name'].str.startswith(file_name_without_extension.split('_')[0]), 'error'] = True 
-                            failed_script_name = scriptFileName 
+                            #failed_script_name = scriptFileName 
                             
 
                         records = pd.DataFrame(result,index=[0])
@@ -149,7 +210,7 @@ def read_files_in_folder(apex_class_with_changes, folder_path, configDF):
 #                             updated_sheet = pd.concat([existing_df, records], ignore_index=True)
 #                             results = updated_sheet
 
-                        print("")
+                        #print("")
             else: 
                 print(f'executing File: {scriptFileName}')
                 resultDf.at[index, errorColumn] = f'Script not Executed'
@@ -159,13 +220,18 @@ def read_files_in_folder(apex_class_with_changes, folder_path, configDF):
     print('Execution Completed')
     return resultDf
 
+
+# In[9]:
+
+
 # Call the function to read files in the folder
 startTime = time.time()
 
-chnages_from_PR = []
+changes_from_PR = []
 for arg in sys.argv[1:]:
-    chnages_from_PR.append(arg)
+    changes_from_PR.append(arg)
 
+#result_list = [file.split('/')[-1] for file in chnages_from_PR if file.startswith('manual-steps-automation/scripts/apex/') and file.endswith('.apex')]
 apex_class_with_changes = [file.split('/')[-1] for file in changes_from_PR if file.endswith('.apex')]
 
 resultDf = read_files_in_folder(apex_class_with_changes, folder_path, configDF) 
@@ -174,6 +240,10 @@ timediff = round((endTime -  startTime)/60,2)
 print(f'Total time to execute the script {timediff}')
 
 resultDf = resultDf[columnsToKeep]
+
+# Split the dataframe into two based on the 'successs' column
+df_success = resultDf[resultDf['successs'] == True]
+df_failure = resultDf[resultDf['successs'] == False]
 
 #Save in CSV
 results_folder_path = releaseName +'_results/'
@@ -188,6 +258,18 @@ if not os.path.exists(results_folder_path):
         print(f"Failed to create results folder at '{results_folder_path}'")
         
 if os.path.exists(results_folder_path):
-    csvFileName = f'{results_folder_path}/'+selected_option+'Result.csv'
-    resultDf.to_csv(csvFileName)
+    csvFileName = f'{results_folder_path}/'+selected_option+'Success Result.csv'
+    df_success.to_csv(csvFileName)
+    
+    csvFileName = f'{results_folder_path}/'+selected_option+'Failure Result.csv'
+    df_failure.to_csv(csvFileName)
     print('Results saved successfully')
+endTime = time.time()
+print(f'Total Time Taken To Run Script: {endTime - startTime}')
+
+
+# In[ ]:
+
+
+
+
